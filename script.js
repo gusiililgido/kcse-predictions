2b86305881cf6d693ff8d016e295dca11992f4a5
gusiililgido <gusiililgido@gmail.com>
2026-05-30 00:42:16 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
 const buyBtn = document.getElementById("buyBtn");
 
-buyBtn.addEventListener("click", () => {
-    alert("STK Push Coming In Step 2");
+buyBtn.addEventListener("click", async () => {
+
+    const phone = prompt("Enter M-PESA Number");
+
+    if (!phone) {
+        alert("Phone number required");
+        return;
+    }
+
+    try {
+
+        const response = await fetch("YOUR_RENDER_BACKEND_URL/pay", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({ phone })
+        });
+
+        const data = await response.json();
+
+        alert(data.message);
+
+    } catch (error) {
+        alert("Server Error");
+    }
 });

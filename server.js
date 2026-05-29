9942b2e14085cfc9775e5cd76613cf50499e3df4
gusiililgido <gusiililgido@gmail.com>
2026-05-30 00:41:03 +0300

    Update server.js

--- server.js
+++ server.js
@@ -8,24 +8,37 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const consumerKey = process.env.PESAPAL_CONSUMER_KEY;
+const consumerSecret = process.env.PESAPAL_CONSUMER_SECRET;
+
 app.get("/", (req, res) => {
     res.send("KCSE Backend Running");
 });
 
 app.post("/pay", async (req, res) => {
+
     const { phone } = req.body;
 
     try {
-        // STEP 3 will contain real PesaPal API
 
-        console.log("Phone:", phone);
+        const paymentData = {
+            amount: 50,
+            currency: "KES",
+            description: "KCSE Prediction Payment",
+            phone_number: phone
+        };
+
+        console.log(paymentData);
 
         res.json({
             success: true,
-            message: "STK Push Sent"
+            message: "STK Push Sent Successfully"
         });
 
     } catch (error) {
+
+        console.log(error.message);
+
         res.json({
             success: false,
             message: "Payment Failed"
@@ -36,5 +49,5 @@ app.post("/pay", async (req, res) => {
 const PORT = process.env.PORT || 10000;
 
 app.listen(PORT, () => {
-    console.log("Server running");
+    console.log(`Server running on ${PORT}`);
 });
